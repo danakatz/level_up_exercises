199aefc9960b8176b078e24b4478d1179cd47302
Dana Katz <dkatz@enova.com>
2015-06-16 09:54:50 -0500

    Add units to weight printout

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -20,6 +20,7 @@ class Dinosaur
 
   def print_var(v)
     var_value = instance_variable_get(v).to_s
+    var_value << " lbs" if v.to_s == "weight"
     var_key = v.to_s[1..-1].capitalize
     puts "#{var_key}: #{var_value}" unless var_value.empty?
   end
